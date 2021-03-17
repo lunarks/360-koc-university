db76efbda8215a9d90730f980141975033eda6e8
lunarks <selinkaya99@gmail.com>
2021-03-17 21:22:02 +0300

    Added localization settings

---
+++ 360 Koc University/Assets/Scripts/LanguageSelectionButtons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LanguageSelectionButtons : MonoBehaviour
+{
+    // Turkish Button clicked
+    public void LoadSceneWithTurkishLang()
+    {
+        SceneManager.LoadScene("Tutorial");
+        Destroy(GameObject.FindWithTag("EnglishText"));
+    }
+
+
+    // English Button clicked
+    public void LoadSceneWithEnglishLang()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void DestroyTurkishText()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("TurkishText");
+        foreach (GameObject enemy in enemies)
+            GameObject.Destroy(enemy);
+    }
+}
