9c40071d8b6b48d111abc83e03a437d333d383f5
akalayci18 <72552136+akalayci18@users.noreply.github.com>
2021-03-17 21:56:27 +0300

    Move Movement Script

--- 360 Koc University/Assets/Scripts/CameraBehaviour.cs
+++ 360 Koc University/Assets/Scripts/CameraBehaviour.cs
@@ -4,71 +4,83 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    // flag to keep track whether we are dragging or not
-    public bool isDragging = false;
 
-    // starting point of a camera movement
-    float startMouseX;
-    float startMouseY;
+	public float turnSpeed = 4.0f;      // Speed of camera turning when mouse moves in along an axis
+	//public float panSpeed = 4.0f;       // Speed of the camera when being panned
+	//public float zoomSpeed = 4.0f;      // Speed of the camera going back and forth
 
-    // Camera component
-    public Camera cam;
+	private Vector3 mouseOrigin;    // Position of cursor when mouse dragging starts
+	//private bool isPanning;     // Is the camera being panned?
+	private bool isRotating;    // Is the camera being rotated?
+	//private bool isZooming;     // Is the camera zooming?
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Get our camera component
-        cam = this.gameObject.GetComponent<Camera>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+	void Update()
+	{
+		// Get the left mouse button
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Get mouse origin
+			mouseOrigin = Input.mousePosition;
+			isRotating = true;
+		}
 
-        // if we press the left button and we haven't started dragging
-        if (Input.GetMouseButtonDown(0) && !isDragging)
-        {
-            // set the flag to true
-            isDragging = true;
+		/*
+		// Get the right mouse button
+		if (Input.GetMouseButtonDown(1))
+		{
+			// Get mouse origin
+			mouseOrigin = Input.mousePosition;
+			isPanning = true;
+		}
+		*/
 
-            // save the mouse starting position
-            startMouseX = Input.mousePosition.x;
-            startMouseY = Input.mousePosition.y;
-        }
-        // if we are not pressing the left btn, and we were dragging
-        else if (Input.GetMouseButtonUp(0) && isDragging)
-        {
-            // set the flag to false
-            isDragging = false;
-        }
-    }
+		/*
+		// Get the middle mouse button
+		if (Input.GetMouseButtonDown(2))
+		{
+			// Get mouse origin
+			mouseOrigin = Input.mousePosition;
+			isZooming = true;
+		}
+		*/
 
-    void LateUpdate()
-    {
-        // Check if we are dragging
-        if (isDragging)
-        {
-            //Calculate current mouse position
-            float endMouseX = Input.mousePosition.x;
-            float endMouseY = Input.mousePosition.y;
+		// Disable movements on button release
+		if (!Input.GetMouseButton(0)) isRotating = false;
+		//if (!Input.GetMouseButton(1)) isPanning = false;
+		//if (!Input.GetMouseButton(2)) isZooming = false;
 
-            //Difference (in screen coordinates)
-            float diffX = endMouseX - startMouseX;
-            float diffY = endMouseY - startMouseY;
+		// Rotate camera along X and Y axis
+		if (isRotating)
+		{
+			Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
 
-            //New center of the screen
-            float newCenterX = Screen.width / 2 + diffX;
-            float newCenterY = Screen.height / 2 + diffY;
+			
+			transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+			transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
 
-            //Get the world coordinate , this is where we want to look at
-            Vector3 LookHerePoint = cam.ScreenToWorldPoint(new Vector3(newCenterX, newCenterY, cam.nearClipPlane));
+		}
+		/*
+	   // Move the camera on it's XY plane
+	   if (isPanning)
+	   {
+		   Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
 
-            //Make our camera look at the "LookHerePoint"
-            transform.LookAt(LookHerePoint);
+		   Vector3 move = new Vector3(pos.x * panSpeed, pos.y * panSpeed, 0);
+		   transform.Translate(move, Space.Self);
+	   }
 
-            //starting position for the next call
-            startMouseX = endMouseX;
-            startMouseY = endMouseY;
-        }
-    }
+	   // Move the camera linearly along Z axis
+	   if (isZooming)
+	   {
+		   Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+		   Vector3 move = pos.y * zoomSpeed * transform.forward;
+		   transform.Translate(move, Space.World);
+	   }
+		*/
+
+	}
+
+
 }
