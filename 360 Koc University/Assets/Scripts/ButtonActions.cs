1164347f764d5c3fc39b1f1ef7ca9b067d19ac70
lunarks <selinkaya99@gmail.com>
2021-03-31 18:55:33 +0300

    Button Actions scripts added

---
+++ 360 Koc University/Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonActions : MonoBehaviour
+{
+    [SerializeField] Image backgroundImage;
+
+    Color32 greenColor = new Color32(100, 196, 171, 255);
+    Color32 redColor = new Color32(193, 11, 37, 255);
+
+    public void RightAnswer()
+    {
+        backgroundImage.color = greenColor;
+    }
+
+    public void WrongAnswer()
+    {
+        backgroundImage.color = redColor;
+    }
+
+}
