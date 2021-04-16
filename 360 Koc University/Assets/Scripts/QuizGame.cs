4d5399d292309e3a8c5b51309c0918b5e6713f4d
lunarks <selinkaya99@gmail.com>
2021-04-16 11:45:38 +0300

    Added SGKM photos

---
+++ 360 Koc University/Assets/Scripts/QuizGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Localization;
+
+public class QuizGame : MonoBehaviour
+{
+    // Question Text
+    
+
+    // Choice Buttons
+    [SerializeField] Button choice1;
+    [SerializeField] Button choice2;
+    [SerializeField] Button choice3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pressNext()
+    {
+        // 
+    }
+}
