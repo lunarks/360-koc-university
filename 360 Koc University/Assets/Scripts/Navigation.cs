eb30685b42e8f44a336a95e653a57b2796e770d8
lunarks <selinkaya99@gmail.com>
2021-04-17 18:38:33 +0300

    Bus Stop scene added, CASE building materials and scene added

---
+++ 360 Koc University/Assets/Scripts/Navigation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+    // Holds all the materials for the scene
+    [SerializeField] Material[] materials;
+
+    // To store the Sphere game object
+    private GameObject sphere;
+
+    void Awake()
+    {
+        // Fetch the Entrance game object
+        sphere = gameObject;
+    }
+
+    public void ChangeMaterial(int i)
+    {
+        sphere.GetComponent<MeshRenderer>().material = materials[i];
+    }
+}
