86e0885b81d2fc70994972e20fc76a9c4d4a976b
akalayci18 <72552136+akalayci18@users.noreply.github.com>
2021-04-29 04:00:26 +0300

    Audio Source and RoadtoGym scene added

---
+++ 360 Koc University/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static AudioManager instance;
+
+    public AudioClip[] s_audiolist;
+
+    List<AudioSource> s_source = new List<AudioSource>();
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    void Start()
+    {
+        for (int i = 0; i < s_audiolist.Length; i++)
+        {
+
+            s_source.Add(new AudioSource());
+            s_source[i] = gameObject.AddComponent<AudioSource>();
+            s_source[i].clip = s_audiolist[i];
+        }
+    }
+
+    public void s_playsound(int s)
+    {
+        s_source[s].Play();
+    }
+}
+        
+    
+
+    
---
+++ 360 Koc University/Assets/Scripts/AudioSource_button.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSource_button : MonoBehaviour
+{
+   public void  play_sound(int s)
+    {
+        AudioManager.instance.s_playsound(s);
+
+    }
+}
