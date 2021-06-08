4e1649c80ab6b06cd0264e3e7395d1a8c791db90
lunarks <selinkaya99@gmail.com>
2021-06-08 19:14:26 +0300

    Created settings canvas and animation

---
+++ 360 Koc University/Assets/Animator/SettingsAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsAnimation : MonoBehaviour
+{
+    public GameObject settingsCanvas;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = settingsCanvas.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void slideInAnimation()
+    {
+        animator.SetBool("openSettings", true);
+    }
+
+    public void slideOutAnimation()
+    {
+        animator.SetBool("openSettings", false);
+    }
+}
