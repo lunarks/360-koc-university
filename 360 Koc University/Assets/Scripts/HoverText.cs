880d34a1ea9066af6c739da4a0799a9b48217f5b
lunarks <selinkaya99@gmail.com>
2021-06-08 19:44:21 +0300

    Updated map canvas

---
+++ 360 Koc University/Assets/Scripts/HoverText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject textHolder;
+
+    private void Start()
+    {
+        textHolder.SetActive(false);
+    }
+
+    //Detect if the Cursor starts to pass over the GameObject
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        //Output to console the GameObject's name and the following message
+        Debug.Log("Cursor Entering " + name + " GameObject");
+        textHolder.SetActive(true);
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        //Output the following message with the GameObject's name
+        Debug.Log("Cursor Exiting " + name + " GameObject");
+        textHolder.SetActive(false);
+    }
+}
