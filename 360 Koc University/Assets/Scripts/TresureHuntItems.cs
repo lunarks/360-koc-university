2d4001d95903941ebc53beaecac888541d901d3a
lunarks <selinkaya99@gmail.com>
2021-06-09 10:30:47 +0300

    Updated the treasure hunt canvas

--- 360 Koc University/Assets/Scripts/TresureHuntItems.cs
+++ 360 Koc University/Assets/Scripts/TresureHuntItems.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class TresureHuntItems : MonoBehaviour
 {
+    public GameObject gameManager;
+
+    public GameObject englishTexts;
+    public GameObject turkishTexts;
+
+    public GameObject trophyQuestionMark;
+    public GameObject trophyModel;
+
+    public GameObject staplerQuestionMark;
+    public GameObject staplerModel;
+
+
+    private bool trophy;
+    private bool stapler;
+    private bool turkish;
+    private bool english;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        trophy = gameManager.GetComponent<GameManager>().isTrophyCollected;
+        stapler = gameManager.GetComponent<GameManager>().isStaplerCollected;
+        turkish = gameManager.GetComponent<GameManager>().isTurkishLangaugeSelected;
+        english = gameManager.GetComponent<GameManager>().isEnglishLanguageSelected;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (turkish)
+        {
+            turkishTexts.SetActive(true);
+            englishTexts.SetActive(false);
+        }
+
+        if (englishTexts)
+        {
+            turkishTexts.SetActive(false);
+            englishTexts.SetActive(true);
+        }
+
+        if (trophy)
+        {
+            trophyQuestionMark.SetActive(false);
+            trophyModel.SetActive(true);
+        }
+
+        if (stapler)
+        {
+            staplerQuestionMark.SetActive(false);
+            staplerModel.SetActive(true);
+        }
     }
 }
