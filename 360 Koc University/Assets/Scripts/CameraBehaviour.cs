6a1bfc0809129cd0721e36d5d790abb495f577b6
akalayci18 <72552136+akalayci18@users.noreply.github.com>
2021-06-10 01:02:24 +0300

    The speed of the camera turns increased

--- 360 Koc University/Assets/Scripts/CameraBehaviour.cs
+++ 360 Koc University/Assets/Scripts/CameraBehaviour.cs
@@ -17,15 +17,17 @@ public class CameraBehaviour : MonoBehaviour
 	float rotationY = 0.0f;
 	float rotationX = 0.0f;
 
-	
+	public float turnSpeed = 4.0f;
+
+
 
 	void Update()
 	{
 
 		if (Input.GetMouseButton(0))
 		{
-			rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
-			rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+			rotationX += Input.GetAxis("Mouse X") * turnSpeed * sensX * Time.deltaTime;
+			rotationY += Input.GetAxis("Mouse Y") * turnSpeed * sensY * Time.deltaTime;
 			rotationY = Mathf.Clamp(rotationY, minY, 90.0f);
 			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		}
